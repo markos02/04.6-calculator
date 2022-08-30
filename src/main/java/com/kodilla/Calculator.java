836d0b695494cd42de89660af7eea399e4505c96
Marcin Kosakowski <kosakowski.m@gmail.com>
2022-08-30 14:28:11 +0200

    Third Commit

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,2 +1,19 @@
-package com.kodilla;public class Calculator {
+package com.kodilla;
+
+public class Calculator {
+
+    public int add (int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    public int substract (int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        System.out.println("Wynik dodawania: " + calculator.add(5,4));
+        System.out.println("Wynik odejmowania: " + calculator.substract(10,2));
+    }
 }
